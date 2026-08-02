dfcd4ec4d71a09530bb89536afeb9d29743cbc91
Kathyops <2737865342@qq.com>
2026-08-02 01:28:40 -0400

    Initial commit: Mechanical Hand URDF

---
+++ src/el_hand/launch/display.launch.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+    # 直接使用绝对路径
+    urdf_path = '/home/kathy/Mechanical-Hand/src/el_hand/urdf/mechanical_hand.urdf'
+    
+    with open(urdf_path, 'r') as f:
+        robot_description = f.read()
+    
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2'
+        )
+    ])
